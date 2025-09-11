72fbfb70e25ecc4b7515a2d7b63c0408fe21c0ca
Vansh Maheshwari <121272010+Vansh98789@users.noreply.github.com>
2025-09-11 18:19:03 +0530

    Update VaultContract.sol

--- VaultContract.sol
+++ VaultContract.sol
@@ -1,28 +1,41 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract VaultContract{
+contract VaultContract {
     uint public totalBalance;
-    mapping (address=>uint) shares;
-    uint totalProfit=10000; // lets assume
+    mapping(address => uint) shares;
+    uint public totalProfit = 10000; // lets assume fixed profit
+    uint public constant PRECISION = 1e18; // scale for decimal shares
 
-    function deposite()public  payable {
-        require(msg.value>0,"you must deposite some amount");
-        totalBalance+=msg.value;
-        uint totalShare=(msg.value)/totalBalance;
-        shares[msg.sender]=totalShare;
+    function deposite() public payable {
+        require(msg.value > 0, "you must deposite some amount");
+        totalBalance += msg.value;
+
+        // store fraction as fixed-point with 1e18 precision
+        uint totalShare = (msg.value * PRECISION) / totalBalance;
+        shares[msg.sender] = totalShare;
     }
-    function withdraw(uint _amount)public {
-        require(shares[msg.sender]>0,"you do not have any money deposited");
-        require(_amount>0,"you must withdraw some amount");
-        require(_amount<=shares[msg.sender]*totalBalance,"you do not have that much money");
 
-        uint moneyWithdraw=_amount+totalProfit*shares[msg.sender];
-        shares[msg.sender]=(shares[msg.sender]-_amount)/totalBalance;
+    function withdraw(uint _amount) public {
+        require(shares[msg.sender] > 0, "you do not have any money deposited");
+        require(_amount > 0, "you must withdraw some amount");
+        require(
+            _amount <= (shares[msg.sender] * totalBalance) / PRECISION,
+            "you do not have that much money"
+        );
+
+        uint moneyWithdraw = _amount + (totalProfit * shares[msg.sender]) / PRECISION;
+
+        // update share after withdrawal
+        shares[msg.sender] =
+            ((shares[msg.sender] * PRECISION) - (_amount * PRECISION) / totalBalance) /
+            PRECISION;
+
         payable(msg.sender).transfer(moneyWithdraw);
-        totalBalance-=_amount;
+        totalBalance -= _amount;
     }
-    function getMyshare()public view  returns (uint ){
-        return shares[msg.sender]*100;
+
+    function getMyshare() public view returns (uint) {
+        return (shares[msg.sender] * 100) / PRECISION; // percentage
     }
 }
